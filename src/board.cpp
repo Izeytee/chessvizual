0fdc3d2b8f31b42f8cdaf4c2e04116d5e03ab84e
Izeytee <I-zey@yandex.ru>
2018-04-08 14:57:36 +0600

    Initiated Start Board

---
+++ src/board.cpp
@@ -0,0 +1,34 @@
+#include<stdlib.h>
+#include<stdio.h>
+#ifndef _MYPROJECT_board_cpp
+#define _MYPROJECT_board_cpp
+void board(char place[][8])
+{
+    place[0][0] = 'R';
+    place[0][1] = 'N'; 
+    place[0][2] = 'B';
+    place[0][3] = 'Q'; 
+    place[0][4] = 'K'; 
+    place[0][5] = 'B';
+    place[0][6] = 'N'; 
+    place[0][7] = 'R';
+    place[7][0] = 'r';
+    place[7][1] = 'n'; 
+    place[7][2] = 'b';
+    place[7][3] = 'q'; 
+    place[7][4] = 'k'; 
+    place[7][5] = 'b';
+    place[7][6] = 'n'; 
+    place[7][7] = 'r';
+    for (int i = 0; i<8; i++)
+       {
+             place[1][i] = 'P';
+             place[6][i] = 'p';
+       }
+    for (int j = 2; j < 6; j++)
+    {
+        for (int i = 0; i<8; i++)
+        place[j][i] = ' ';
+    }
+}
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,10 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <board.cpp>
 
 int main()
 
-{   
+{
     char place[8][8];
-    place[0][0] = 'R';
-    place[0][1] = 'N'; 
-    place[0][2] = 'B';
-    place[0][3] = 'Q'; 
-    place[0][4] = 'K'; 
-    place[0][5] = 'B';
-    place[0][6] = 'N'; 
-    place[0][7] = 'R';
-    place[7][0] = 'r';
-    place[7][1] = 'n'; 
-    place[7][2] = 'b';
-    place[7][3] = 'q'; 
-    place[7][4] = 'k'; 
-    place[7][5] = 'b';
-    place[7][6] = 'n'; 
-    place[7][7] = 'r';
-    for (int i = 0; i<8; i++)
-       {
-             place[1][i] = 'P';
-             place[6][i] = 'p';
-       }
-    for (int j = 2; j < 6; j++)
-    {
-        for (int i = 0; i<8; i++)
-        place[j][i] = ' ';
-    }
+    board(place);
     for (int i = 0; i<8; i++)
     {
         for (int j = 0; j < 8; j++)
