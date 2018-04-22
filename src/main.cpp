8e1df514d46c92dc6276d301c4d0619cb091779c
Izeytee <I-zey@yandex.ru>
2018-04-22 13:58:17 +0600

    Remove fanction

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
-#include "board.h"
-#include "board_print_plain.h"
+#include "board.cpp"
+#include "board_print_plain.cpp"
 
 int main()
 {   
     char place[8][8];
     board(place); 
-    for (int i = 0; i<8; i++)
-    {
-        printf("%d ",i+1);
-        for (int j = 0; j < 8; j++)
-        {
-           printf("%c ", place[i][j]);
-        }
-        printf("\n");
-    }
-    printf("  A B C D E F G H\n");
+    printboard(place);
     board_print_plain(place);
     char Move[5];
     while(1)
